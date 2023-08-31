7e070465bac89469203b109909ca3afe1ae280de
B-L SARATH <leelsarathbaswa@gmail.com>
2023-08-31 23:24:53 +0530

    imagelink

--- src/contactappredux/Homepage.js
+++ src/contactappredux/Homepage.js
@@ -3,7 +3,7 @@ import {Link } from "react-router-dom";
 function Homepage() {
   return (
     <div className="text-center container mt-5 p-5">
-    <img src="https://tse4.mm.bing.net/th?id=OIP.upVWr4cu4yWvsQwLuYyprQHaHa&pid=Api&P=0" alt="contact img" style={{width:"170px"}}/>
+    <img src="https://cdn.onlinewebfonts.com/svg/img_453507.png" alt="contact img" style={{width:"170px"}}/>
       <h1 >Contacts application</h1>
       <Link to ="/contacts" className="btn contactbtn"> Get started </Link>
     </div>
